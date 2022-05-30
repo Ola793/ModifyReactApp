98adda71a0e62a259fda7563cbcfa6b5991476a3
Olha Teplova <67291449+Ola793@users.noreply.github.com>
2022-05-30 23:28:52 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import Logger from "redux-logger";
+import thunk from "redux-thunk";
 import { rootReducer } from "./reducers/rootReducer";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk, Logger));
 
 export default store;
